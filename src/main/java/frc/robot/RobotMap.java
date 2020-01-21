69fb2f79b30ab02bf8370572b381e310e93ff06b
ZYXWong24680 <wongz1@bxscience.edu>
2020-01-21 15:33:04 -0500

    cleaned climber branch

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -8,26 +8,9 @@
 package frc.robot;
 
 /**
-<<<<<<< HEAD
-* Add your docs here.
-*/
-public class RobotMap {
-    public static int turretPort = 0;
-    public static int hoodPort = 1;
-    public static int shooterPort = 2; 
-    
-    public static int beltPort1 = 3;
-    public static int beltPort2 = 4;
-    public static int beltPort3 = 5;
-    public static int beltPort4 = 6; 
-
-    public static int intake = 8;
-    public static int lift = 9; 
-=======
  * Add your docs here.
  */
 public class RobotMap {
     public static int joyPort = 0;
     public static int climberPort = 0;
->>>>>>> 507af922ab33ce81fe348082e50c2c3bfc315837
 }
