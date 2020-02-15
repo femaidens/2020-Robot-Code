0d95803e9aefe0c0fb9aa2e606a08a7495a5b10e
FIRST_NAME LAST_NAME <wongm8@bxscience.edu>
2020-02-15 10:08:36 -0500

    Updated piston

--- src/main/java/frc/robot/subsystems/Climb.java
+++ src/main/java/frc/robot/subsystems/Climb.java
@@ -11,7 +11,9 @@ import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.Servo;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import frc.robot.OI;
 import frc.robot.Robot;
@@ -22,26 +24,30 @@ public class Climb extends Subsystem {
   /**
    * Creates a new Climber.
    */
-  private static final double UP_VALUE = 0.0;
-	private static final double DOWN_VALUE = 5.0;
+  //private static final double UP_VALUE = 0.0;
+	//private static final double DOWN_VALUE = 5.0;
 
   public static CANSparkMax climbMotor = new CANSparkMax(RobotMap.climbMotorPort, MotorType.kBrushless);
-	
+  
+  //true means not hit and false means hit
 	public static DigitalInput limitSwitchB = new DigitalInput(RobotMap.limitPortB); //bottom
 	public static DigitalInput limitSwitchT = new DigitalInput(RobotMap.limitPortT); //top
 	
-	public static Servo servo = new Servo(RobotMap.servoPort);
+  //public static Servo servo = new Servo(RobotMap.servoPort);
+  public static DoubleSolenoid climbSol = new DoubleSolenoid(RobotMap.solPortF, RobotMap.solPortR);
 
   public Climb() {
   }
 	
 	public static void move(){
 		double value = OI.atkJoy2.getRawAxis(1);
-		if(value>0 && !limitSwitchT.get()){
-			servo.setPosition(UP_VALUE);
+		if(value>0 && limitSwitchT.get()){
+      //servo.setPosition(UP_VALUE);
+      climbSol.set(Value.kReverse);
 			climbMotor.set(value);
-		}else if(value<0 && !limitSwitchB.get()){
-			servo.setPosition(DOWN_VALUE);
+		}else if(value<0 && limitSwitchB.get()){
+      //servo.setPosition(DOWN_VALUE);
+      climbSol.set(Value.kForward);
       climbMotor.set(value);
 		}
 	}
